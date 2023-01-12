9835e44676bcfe721205a0e63e6ae4819018414c
cameronjellis <cameron.ellis@siliconmtn.com>
2023-01-11 20:32:55 -0700

    rendering properly - bootstrap css

--- src/root.component.js
+++ src/root.component.js
@@ -1,7 +1,12 @@
+import Navbar from "./components/Navbar";
+
 export default function Root(props) {
   return (
     <div>
-      <h1>Graph Visualization</h1>
+      <div>
+        <Navbar />
+      </div>
+      <h1>Graph Visualizations!</h1>
       <section>{props.name} is mounted!</section>
     </div>
   );
--- src/smtsinglespa-graph.js
+++ src/smtsinglespa-graph.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import singleSpaReact from "single-spa-react";
+import Navbar from "./components/Navbar";
 import Root from "./root.component";
 
 const lifecycles = singleSpaReact({
